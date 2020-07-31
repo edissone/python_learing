c87586d7970d6a03391bb4fd27be78db82004f3c
edissone <eduard.prog@gmail.com>
2020-07-31 17:37:34 +0300

    Created Application class, updated readme.md

---
+++ apps/Application.py
@@ -0,0 +1,90 @@
+from abc import ABC, abstractmethod
+from datetime import date
+from enum import Enum
+
+
+class Application(ABC):
+    __slots__ = ('_name', '_release_date', '_version', '_desc')
+
+    def __init__(self, name: str, release_date: date, version: str):
+        if not isinstance(name, str) \
+                or not isinstance(release_date, date) \
+                or not isinstance(version, str):
+            raise AttributeError("Invalid attribute")
+        self._name = name
+        self._release_date = release_date
+        self._version = version
+        self._desc = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        if not isinstance(value, str):
+            raise AttributeError("Invalid attribute")
+        self._name = value
+
+    @property
+    def release_date(self):
+        return self._release_date
+
+    @release_date.setter
+    def release_date(self, value: date):
+        if not isinstance(value, date):
+            raise AttributeError("Invalid attribute")
+        self._release_date = value
+
+    @property
+    def version(self):
+        return self._version
+
+    @version.setter
+    def version(self, value: str):
+        if not isinstance(value, str):
+            raise AttributeError("Invalid attribute")
+        self._version = value
+
+    @property
+    def desc(self):
+        return self._desc
+
+    @desc.setter
+    def desc(self, value: str):
+        if not isinstance(value, str):
+            raise AttributeError("Invalid attribute")
+        self._desc = value
+
+    def __str__(self):
+        return f"Application {self._name} {self._version}, released {self._release_date}"
+
+    @abstractmethod
+    def __repr__(self):
+        return f"<{self.__class__.__name__}(name={self._name}," \
+               f" release_date={self._release_date}," \
+               f" version={self._version})>"
+
+    def __eq__(self, other):
+        if not isinstance(other, Application):
+            raise TypeError("Attribute must be the same")
+        if self.release_date == other.release_date:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        if not isinstance(other, Application):
+            raise TypeError("Attribute must be the same")
+        if self.release_date > other.release_date:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        if not isinstance(other, Application):
+            raise TypeError("Attribute must be the same")
+        if self.release_date < other.release_date:
+            return True
+        else:
+            return False
